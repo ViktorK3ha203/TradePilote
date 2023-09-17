74f83e3caa9b6d37e1817c4051304f0c3674bec7
vk <viktor.khankov@gmail.com>
2023-09-17 22:36:35 +0300

    README.md edits and code cleaning

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,25 +1,26 @@
-import React, { Suspense, useState, useContext } from 'react';
+import { Suspense } from 'react';
 import './styles/index.scss';
 import { Route, Routes } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTheme } from './providers/ThemeProvider';
-import { AboutPage } from 'pages/AboutPage';
 import { MainPage } from 'pages/MainPage';
 
 const App = () => {
-const {theme, toggleTheme} = useTheme();
+    const { theme, toggleTheme } = useTheme();
     return (
         <div className={classNames('app', {}, [theme])}>
-            <button onClick={toggleTheme}>Toggle</button>
-            <Link to={'/'}>Главная страница</Link>
-            <Link to={'/about'}>О сайте</Link>
-            <Suspense fallback={<div>Loading...</div>}>
-                <Routes>
-                    <Route path={'/about'} element={<AboutPage/>}/>
-                    <Route path={'/'} element={<MainPage/>}/>
-                </Routes>
-            </Suspense>
+            <header className='header'>
+                <Link to={'/'}>Main page</Link>
+                <div className='theme-switcher' onClick={toggleTheme}><span>Toggle</span></div>
+            </header>
+            <main>
+                <Suspense fallback={<div>Loading...</div>}>
+                    <Routes>
+                        <Route path={'/'} element={<MainPage />} />
+                    </Routes>
+                </Suspense>
+            </main>
         </div>
     )
 }
